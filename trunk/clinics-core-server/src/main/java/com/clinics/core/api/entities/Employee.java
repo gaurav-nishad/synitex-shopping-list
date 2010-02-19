1d3d9c1d887e222df3b99d4b6bd9cbcb97a6e52e
leonidms@gmail.com <leonidms@gmail.com@b8dda53c-e189-11de-a668-6fd11605f5ce>
2010-02-19 19:14:11 +0000

    Entity cleanup

--- trunk/clinics-core-server/src/main/java/com/clinics/core/api/entities/Employee.java
+++ trunk/clinics-core-server/src/main/java/com/clinics/core/api/entities/Employee.java
@@ -1,16 +1,10 @@
 package com.clinics.core.api.entities;
 
-import java.util.Date;
-
-import javax.persistence.Basic;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @SequenceGenerator(name = "seq", sequenceName = "EM_EMPLOYEE_SEQ", allocationSize = 1)
 @Entity(name = "Employee")
@@ -20,26 +14,10 @@ public class Employee extends Person implements java.io.Serializable {
 
     private static final long serialVersionUID = 4960041525303686562L;
 
-    @Basic(optional = true)
-    @Temporal(TemporalType.DATE)
-    @Column(name = "DATE_OF_HIRE", insertable = true, updatable = true)
-    private Date dateOfHire;
-
-    @Basic(optional = true)
-    @Column(name = "DIVISION", insertable = true, updatable = true, length = 254)
-    private String division;
-
-    @Basic(optional = true)
-    @Column(name = "AREA", insertable = true, updatable = true, length = 254)
-    private String area;
-
-    @Basic(optional = true)
-    @Column(name = "POSITION", insertable = true, updatable = true, length = 255)
-    private String position;
-
-    @Basic(optional = true)
-    @Column(name = "BUSINESS_TITLE", insertable = true, updatable = true, length = 255)
-    private String businessTitle;
+    // @Basic(optional = true)
+    // @Temporal(TemporalType.DATE)
+    // @Column(name = "DATE_OF_HIRE", insertable = true, updatable = true)
+    // private Date dateOfHire;
 
     // @ManyToOne(fetch = FetchType.LAZY)
     // @JoinColumn(name = "EMPLOYEES_DEPARTMENT_ID", referencedColumnName = "ID", nullable = true)
