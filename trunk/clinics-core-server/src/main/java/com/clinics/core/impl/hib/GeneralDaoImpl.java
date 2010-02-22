7be60436a634294e27900a2ae4c3f20818a16868
leonidms@gmail.com <leonidms@gmail.com@b8dda53c-e189-11de-a668-6fd11605f5ce>
2010-02-22 21:16:17 +0000

    Transaction management tests added

--- trunk/clinics-core-server/src/main/java/com/clinics/core/impl/hib/GeneralDaoImpl.java
+++ trunk/clinics-core-server/src/main/java/com/clinics/core/impl/hib/GeneralDaoImpl.java
@@ -16,8 +16,6 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate3.SessionFactoryUtils;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import com.clinics.core.api.dao.GeneralDao;
@@ -27,19 +25,18 @@ import com.clinics.core.api.entities.OcEntity;
  * @author Leonids M<leonidms@gmail.com>
  * 
  */
-@Repository
-@Transactional
 public class GeneralDaoImpl<U> implements GeneralDao<U> {
     private static final String DAO_SHOULD_BE_GENERALIZED = "DAO class should be generalized";
 
     @Autowired
     private SessionFactory sessionFactory;
 
+    private transient Class<? extends OcEntity> calculatedHibernateType = null;
+
     /**
      * {@inhetitedDoc}
      */
     @Override
-    @Transactional(readOnly = true)
     public List<U> findByAttributes(Object... attributes) {
         return fetchEntityByAttributes(getHibernateType(), attributes);
     }
@@ -48,14 +45,12 @@ public class GeneralDaoImpl<U> implements GeneralDao<U> {
      * {@inhetitedDoc}
      */
     @Override
-    @Transactional(readOnly = true)
-    public U get(Integer id) {
+    public U get(Long id) {
         return fetchEntity(getHibernateType(), id);
     }
 
     @SuppressWarnings("unchecked")
     @Override
-    @Transactional(readOnly = true)
     public List<U> getAll() {
         return getCurrentSession().createCriteria(getHibernateType()).list();
     }
@@ -72,16 +67,17 @@ public class GeneralDaoImpl<U> implements GeneralDao<U> {
      * {@inhetitedDoc}
      */
     @Override
-    public void store(U entity) {
-        storeEntity(entity);
+    public U store(U entity) {
+        return storeEntity(entity);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public void storeAll(Collection<U> entities) {
+    public Collection<U> storeAll(Collection<U> entities) {
         Assert.notEmpty(entities, "Entities collection must contain elements");
         Assert.notNull(entities, "Entities should not contain null elements");
 
-        getCurrentSession().merge(entities);
+        return (Collection<U>) getCurrentSession().merge(entities);
     }
 
     private DataAccessException castHibernateException(HibernateException e) {
@@ -89,9 +85,9 @@ public class GeneralDaoImpl<U> implements GeneralDao<U> {
     }
 
     @SuppressWarnings("unchecked")
-    protected U fetchEntity(Class<? extends OcEntity> hibernateType, Integer id) {
+    protected U fetchEntity(Class<? extends OcEntity> hibernateType, Long id) {
         try {
-            Assert.notNull(id, "id should be null");
+            Assert.notNull(id, "id should not be null");
             Assert.isAssignable(OcEntity.class, hibernateType);
             return (U) getCurrentSession().load(hibernateType, id);
         } catch (final HibernateException e) {
@@ -120,28 +116,37 @@ public class GeneralDaoImpl<U> implements GeneralDao<U> {
         } catch (final HibernateException e) {
             throw castHibernateException(e);
         }
-    }
+    };
 
     protected Session getCurrentSession() {
         return sessionFactory.getCurrentSession();
-    };
-
-    protected Class<? extends OcEntity> getHibernateType() {
-        final ParameterizedType type = (ParameterizedType) getClass().getGenericSuperclass();
-        Assert.notNull(type, DAO_SHOULD_BE_GENERALIZED);
-        final Type[] typeInfo = type.getActualTypeArguments();
-        Assert.notEmpty(typeInfo, DAO_SHOULD_BE_GENERALIZED);
-
-        @SuppressWarnings("unchecked")
-        final Class<? extends OcEntity> hibernateType = (Class<? extends OcEntity>) typeInfo[0];
-        Assert.isAssignable(OcEntity.class, hibernateType);
-        return hibernateType;
     }
 
-    protected void storeEntity(U entity) {
+    protected Class<? extends OcEntity> getHibernateType() {
+        if (calculatedHibernateType == null) {
+            synchronized (this) {
+                if (calculatedHibernateType == null) {
+                    final ParameterizedType type = (ParameterizedType) getClass().getGenericSuperclass();
+                    Assert.notNull(type, DAO_SHOULD_BE_GENERALIZED);
+                    final Type[] typeInfo = type.getActualTypeArguments();
+                    Assert.notEmpty(typeInfo, DAO_SHOULD_BE_GENERALIZED);
+
+                    @SuppressWarnings("unchecked")
+                    final Class<? extends OcEntity> hibernateType = (Class<? extends OcEntity>) typeInfo[0];
+                    Assert.isAssignable(OcEntity.class, hibernateType);
+                    calculatedHibernateType = hibernateType;
+                }
+            }
+        }
+
+        return calculatedHibernateType;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected U storeEntity(U entity) {
         Assert.notNull(entity, "Could not persist null entity");
         try {
-            getCurrentSession().merge(entity);
+            return (U) getCurrentSession().merge(entity);
         } catch (final HibernateException e) {
             throw castHibernateException(e);
         }
